79abc412a271b6134a5be60581e3f382c992fe23
Daniel Frick <danielfrick7@gmail.com>
2016-12-05 17:19:57 -0600

    Assign-11-AV Completed

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,22 @@
      ]
  };
 
+ var albumAcdc = {
+     title: 'Back in Black',
+     artist: 'ACDC',
+     label: 'Atlantic',
+     year: '1980',
+     albumArtUrl: 'assets/images/album_covers/ACDC.png',
+     songs: [
+         { title: 'Hells Bells', duration: '5:10' },
+         { title: 'Shoot to Thrill', duration: '5:17' },
+         { title: 'Back in Black', duration: '4:14'},
+         { title: 'You Shook Me All Night Long', duration: '3:30' },
+         { title: 'Rock and Roll Aint Noise Pollution', duration: '2:15'}
+     ]
+ };
+
+
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -42,28 +58,23 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
+//Select all of the HTML elements required to display on the album page: title, artist, release info, image, and song list. We want to populate these elements with information. To do so, we assign the corresponding values of the album objects' properties to the HTML elements.
+    
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 var setCurrentAlbum = function(album) {
-     //Select all of the HTML elements required to display on the album page: title, artist, release info, image, and song list. We want to populate these elements with information. To do so, we assign the corresponding values of the album objects' properties to the HTML elements.
-    
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
-     //The firstChild property identifies the first child node of an element, and  nodeValue returns or sets the value of a node. Alternatively, we could technically use  innerHTML to insert plain text (like we did in collection.js), but it's excessive and semantically misleading in this context because we aren't adding any HTML.
-    
+//The firstChild property identifies the first child node of an element, and  nodeValue returns or sets the value of a node. Alternatively, we could technically use  innerHTML to insert plain text (like we did in collection.js), but it's excessive and semantically misleading in this context because we aren't adding any HTML.
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
- 
      //When the Collection view populated with albums, it initially set the value of the parent container's innerHTML to an empty string. This ensured that we were working with a clean slate. The same here
-    
      albumSongList.innerHTML = '';
- 
      //Go through all the songs from the specified album object and insert them into the HTML using the innerHTML property. The createSongRow function is called at each loop, passing in the song number, name, and length arguments from our album object.
-    
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
@@ -71,4 +82,15 @@ var setCurrentAlbum = function(album) {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     
+     var albums = [albumPicasso, albumMarconi, albumAcdc];
+     var index = 1;
+     
+     albumImage.addEventListener("click", function(event){
+         setCurrentAlbum(albums[index]);
+         index++;
+         if(index == albums.length) {
+         index =0;
+         }
+    });
  };
