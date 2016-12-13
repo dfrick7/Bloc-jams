fe1d5deb041ec3612e443f2c1524cd3919b3157e
Daniel Frick <danielfrick7@gmail.com>
2016-12-13 00:34:01 -0600

    CP-19-NP Completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+var albumAcdc = {
+     title: 'Back in Black',
+     artist: 'ACDC',
+     label: 'Atlantic',
+     year: '1980',
+     albumArtUrl: 'assets/images/album_covers/ACDC.png',
+     songs: [
+         { title: 'Hells Bells', duration: '5:10' },
+         { title: 'Shoot to Thrill', duration: '5:17' },
+         { title: 'Back in Black', duration: '4:14'},
+         { title: 'You Shook Me All Night Long', duration: '3:30' },
+         { title: 'Rock and Roll Aint Noise Pollution', duration: '2:15'}
+     ]
+ };
